22470fe7cd1e30ad27b973c538315be4578c9215
Bentley.Hudson <Bentley.Hudson@ustxgpl4307.us.hsi.local>
2020-02-26 10:32:42 -0600

    indy colors

--- globaldata/batchpick.php
+++ globaldata/batchpick.php
@@ -13,30 +13,41 @@ $prepickcutoff = 1820;
 include '../timezoneset.php';
 
 $batches = $conn1->prepare("SELECT 
-                                                            batchtime_cart AS BATCHCART,
-                                                            CAST(batchtime_time_totaltime AS UNSIGNED) AS BATCHTIME,
-                                                            batchtime_printdatetime as PRINTTIME
-                                                        FROM
-                                                            printvis.looselines_batchtime
-                                                                LEFT JOIN
-                                                            voice_batchespicked ON voice_whse = batchtime_whse
-                                                                AND voice_batch = batchtime_cart
-                                                        WHERE
-                                                            batchtime_printdatetime >= case when $whsesel = 3 then  DATE_SUB(NOW(), INTERVAL 6 HOUR) when $whsesel = 7 then DATE_SUB(NOW(), INTERVAL 4 HOUR) else DATE_SUB(NOW(), INTERVAL 3 HOUR) end
-                                                                AND batchtime_whse = $whsesel
-                                                                AND voice_userid = 0
-                                                                AND batchtime_count_line > 5
-                                                                AND batchtime_colgcount / batchtime_count_line <= .95
-                                                                AND voice_cartconfig <> ' '
-                                                                AND batchtime_count_ice / batchtime_count_line <> 1
-                                                        ORDER BY batchtime_cart");
+                                batchtime_cart AS BATCHCART,
+                                CAST(batchtime_time_totaltime AS UNSIGNED) AS BATCHTIME,
+                                batchtime_printdatetime AS PRINTTIME,
+                                MIN(cutoff_zone) AS cutoff_zone,
+                                cutoff_color,
+                                cutoff_color_text
+                            FROM
+                                printvis.looselines_batchtime
+                                    LEFT JOIN
+                                printvis.voice_batchespicked ON voice_whse = batchtime_whse
+                                    AND voice_batch = batchtime_cart
+                                    LEFT JOIN
+                                printvis.printcutoff ON cutoff_DC = batchtime_whse
+                                    AND cutoff_rank = batchtime_shipzone
+                            WHERE
+                                batchtime_printdatetime >= CASE
+                                    WHEN 2 = 3 THEN DATE_SUB(NOW(), INTERVAL 6 HOUR)
+                                    WHEN 2 = 7 THEN DATE_SUB(NOW(), INTERVAL 4 HOUR)
+                                    ELSE DATE_SUB(NOW(), INTERVAL 3 HOUR)
+                                END
+                                    AND batchtime_whse = $whsesel
+                                    AND voice_userid = 0
+                                    AND batchtime_count_line > 5
+                                    AND batchtime_colgcount / batchtime_count_line <= .95
+                                    AND voice_cartconfig <> ' '
+                                    AND batchtime_count_ice / batchtime_count_line <> 1
+                            GROUP BY batchtime_cart , CAST(batchtime_time_totaltime AS UNSIGNED) , batchtime_printdatetime , cutoff_color
+                            ORDER BY batchtime_cart");
 $batches->execute();
 $batches_array = $batches->fetchAll(pdo::FETCH_ASSOC);
 ?>
 
 <div class="row">
     <div id="tablecontainer" class="col-sm-4" style="cursor: default">
-        <table id="shifttable" class="table table-bordered table-striped" cellspacing="0" >
+        <table id="shifttable" class="table table-bordered" cellspacing="0" >
             <thead>
                 <tr>
                     <th style="font-size: 20px; font-family: Calibri; text-align: center">BATCH</th>
@@ -45,11 +56,22 @@ $batches_array = $batches->fetchAll(pdo::FETCH_ASSOC);
             </thead>
             <?php
             foreach ($batches_array as $key => $value) {
-
+                $style_backcolor = ''; //reset style color
+                $style_textcolor = ''; //reset style color
                 if (intval(date('H:i', strtotime($batches_array[$key]['PRINTTIME']))) >= $prepickcutoff) {
-                    $rowclass = 'greenbackground';
+                    $style_backcolor = 'background-color: #0000ff80 !important;';
+                    $style_textcolor = 'color: #000000 !important;';
+                    $rowclass = '';
                 } else {
                     $rowclass = '';
+                }
+
+                //set the color for printcutoff
+                $backcolor = $batches_array[$key]['cutoff_color'];
+                $textcolor = $batches_array[$key]['cutoff_color_text'];
+                if (!is_null($backcolor)) {
+                    $style_backcolor = 'background-color: ' . $backcolor . ' !important;';
+                    $style_textcolor = 'color:' . $textcolor . '!important;';
                 }
 
                 //close out table and create new table every 30 batches
@@ -59,7 +81,7 @@ $batches_array = $batches->fetchAll(pdo::FETCH_ASSOC);
                 </table>
             </div>
             <div id="tablecontainer" class="col-sm-4" style="cursor: default">
-                <table id="shifttable" class="table table-bordered table-striped" cellspacing="0" >
+                <table id="shifttable" class="table table-bordered" cellspacing="0" >
                     <thead>
                         <tr>
                             <th style="font-size: 20px; font-family: Calibri; text-align: center">BATCH</th>
@@ -69,8 +91,9 @@ $batches_array = $batches->fetchAll(pdo::FETCH_ASSOC);
                     <?php
                 }
                 echo'<tr>';
-                echo '<td class="' . $rowclass . '" style="font-size: 20px; font-family: Calibri; text-align: center"><b>' . $batches_array[$key]['BATCHCART'] . "</b></td>";
-                echo '<td class="' . $rowclass . '" style="font-size: 20px; font-family: Calibri; text-align: center"><b>' . $batches_array[$key]['BATCHTIME'] . "</b></td>";
+                echo '<td class="' . $rowclass . '" style="font-size: 20px; font-family: Calibri; text-align: center;' . $style_backcolor . ';' . $style_textcolor . ' "><b>' . $batches_array[$key]['BATCHCART'] . "</b></td>";
+                echo '<td class="' . $rowclass . '" style="font-size: 20px; font-family: Calibri; text-align: center;' . $style_backcolor . ';' . $style_textcolor . ' "><b>' . $batches_array[$key]['BATCHTIME'] . "</b></td>";
+
                 echo'</tr>';
             }
             ?>
