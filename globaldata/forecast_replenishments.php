84fba379a27064f8efd6393415c80fe18c0384d9
Jeromy.Moore <Jeromy.Moore@USPADEL4010W10.us.hsi.local>
2020-05-13 10:21:07 -0400

    putaway display changes with moves display

--- globaldata/forecast_replenishments.php
+++ globaldata/forecast_replenishments.php
@@ -299,9 +299,3 @@ $totalconsol = $array_totalconsol[0]['TIME_CONSOL'];
 
 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
